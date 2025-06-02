4d41b8e6e8cf29565f93b7c1681a019532c5490f
Matej Kvassay <matejkvassay@icloud.com>
2025-06-02 10:30:35 +0200

    docstr

--- llmbrix/exceptions.py
+++ llmbrix/exceptions.py
@@ -1,4 +1,6 @@
 class PromptRenderError(Exception):
-    """Raised when render or partial_render receives invalid or mismatched variables."""
+    """
+    Raised when render or partial_render receives invalid or mismatched variables.
+    """
 
     pass
