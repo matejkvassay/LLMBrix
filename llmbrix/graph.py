b0ad3c525ae0b6e215467b7b9b99cfb6f6499ece
Matej Kvassay <matejkvassay@icloud.com>
2025-11-24 09:57:23 +0100

    wip

--- llmbrix/graph.py
+++ llmbrix/graph.py
@@ -1,5 +1,4 @@
 import uuid
-from collections import defaultdict
 from typing import Callable
 
 
@@ -10,73 +9,74 @@ class State:
     def __getitem__(self, item):
         return self.read(item)
 
-    def write(self, key=None, value=None, **kwargs):
-        if key:
-            self.data[key] = value
-        else:
-            self.data.update(kwargs)
+    def write(self, **kwargs):
+        self.data.update(kwargs)
 
     def read(self, key):
         return self.data[key]
 
 
 class Node:
-    def __init__(self, name: str, func: Callable[[State], None]):
-        self.uid = uuid.uuid4()
-        self.name = name
+    def __init__(self, func: Callable[[State], None], name: str = None):
+        self.uid = uuid.uuid4().int
+        self.name = name or func.__qualname__
         self.func = func
 
     def run(self, state: State):
         self.func(state)
 
+    def __hash__(self):
+        return hash(self.uid)
 
-class Edge:
-    def __init__(self, src: Node, dst: Node, cond: Callable[[State], None] = None):
-        self.src = src
-        self.dst = dst
-        self.cond = cond
+    def __str__(self):
+        return f"{self.name}_{self.uid}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.uid == other.uid
+
+
+class RouterNode:
+    def __init__(self, state_key: str, node_map: dict[str, Node], name: str = None):
+        self.uid = uuid.uuid4().int
+        self.name = name or f"router_{state_key}"
+        self.state_key = state_key
+        self.node_map = node_map
+
+    def run(self, state: State) -> Node:
+        value = state.read(self.state_key)
+        return self.node_map[value]
+
+    def __hash__(self):
+        return hash(self.uid)
+
+    def __str__(self):
+        return f"{self.name}_{self.uid}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return isinstance(other, RouterNode) and self.uid == other.uid
 
 
 class Graph:
-    def __init__(self, start_node: Node, edges: list[tuple[Node, Node] | tuple[Node, Node, Callable[[State], bool]]]):
+    def __init__(self, start_node: Node | RouterNode, edges: list[tuple[Node | RouterNode, Node | RouterNode]]):
         self.start_node = start_node
-
-        self.conditions = dict()
-        self.successors = defaultdict(list)
-        # add check - if more than 1 successor => all of them have to define condition
-        # check if conditional edge has more than 1 destinations (otherwise it will end exec)
-        for edge in edges:
-            if len(edge) == 2:
-                src, dst, cond = edge[0], edge[1], None
-            elif len(edge) == 3:
-                src, dst, cond = edge
-            else:
-                raise ValueError("Edge has to be either tuple of (Node, Node) or triple of (Node,Node,Callable)")
-            self.successors[src].append(dst)
-            if cond:
-                self.conditions[(src, dst)] = cond
-            # add check - if more than 1 successor => all of them have to define condition
-            # check if conditional edge has more than 1 destinations (otherwise it will end exec)
+        self.successors = {u: v for (u, v) in edges}
 
     def run(self, state: State):
-        step = 0
         node = self.start_node
         while node:
-            step += 1
-            state.write("step_no", step)
-            node.run(state)
-            successors = self.successors.get(node)
-            next_node = None
-            if not successors:
-                state.write({"finished": True, "finished_on": str(node)})
-            elif len(successors) == 1:
-                next_node = successors[0]
+            if isinstance(node, Node):
+                node.run(state)
+                if node in self.successors:
+                    node = self.successors[node]
+                else:
+                    break
+            elif isinstance(node, RouterNode):
+                node = node.run(state)
             else:
-                for v in successors:
-                    if self.conditions[(node, v)]:
-                        next_node = v
-                        break
-                if not next_node:
-                    raise ValueError("All conditions returned False.")
-            state.write("next_node", str(next_node) if next_node else None)
-            node = next_node
+                raise TypeError(f"Unrecognized graph node type: {type(node)}")
