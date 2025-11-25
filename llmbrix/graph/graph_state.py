af170a9fee4c41904cd71aba7fd4f2be4af038b6
Matej Kvassay <matejkvassay@icloud.com>
2025-11-25 08:46:54 +0100

    wip

--- llmbrix/graph/graph_state.py
+++ llmbrix/graph/graph_state.py
@@ -4,27 +4,47 @@ class GraphState:
     Serves to either read or write inputs and outputs from NodeBase execution.
     """
 
-    def __init__(self, data=None):
-        self.data = data or dict()
-
-    def __getitem__(self, item):
-        return self.read(item)
+    def __init__(self, data: dict = None):
+        super().__setattr__("_data", data or {})
+        super().__setattr__("_protected_attrs", frozenset(self.__dict__.keys()))
 
     def write(self, **kwargs):
         """
+        Write kwargs to internal storage.
+
         :param kwargs: Keys are used as keys in internal storage, values as values to be stored.
         """
-        self.data.update(kwargs)
+        self._data.update(kwargs)
 
     def read(self, key):
         """
+        Read value under a key in the internal storage.
+
         :param key: Key to read value from. If key not found KeyError is raised.
         :return: Value stored under the key.
         """
         try:
-            return self.data[key]
+            return self._data[key]
         except KeyError as e:
             raise KeyError(f"GraphState no value stored under key: '{key}'") from e
 
+    def __getitem__(self, item):
+        return self.read(item)
+
+    def __setitem__(self, key, value):
+        self.write(**{key: value})
+
+    def __getattr__(self, name):
+        try:
+            return self._data[name]
+        except KeyError:
+            raise AttributeError(f"GraphState has no attribute or key '{name}'")
+
+    def __setattr__(self, name, value):
+        if name in self._protected_attrs:
+            super().__setattr__(name, value)
+        else:
+            self.write(**{name: value})
+
     def __repr__(self):
-        return f"GraphState(dict_keys={self.data.keys()})"
+        return f"GraphState(dict_keys={self._data.keys()})"
