97fb4ac20ba8810219bce0a49d4f9f62690a1790
Matej Kvassay <matejkvassay@icloud.com>
2025-12-27 08:53:02 +0100

    model msg

--- llmbrix/msg/model_msg_segment.py
+++ llmbrix/msg/model_msg_segment.py
@@ -1,9 +1,11 @@
-from typing import Any, TypedDict
+from dataclasses import dataclass
+from typing import Any
 
 from llmbrix.msg.model_msg_segment_types import ModelMsgSegmentTypes
 
 
-class ModelMsgSegment(TypedDict):
+@dataclass
+class ModelMsgSegment:
     type: ModelMsgSegmentTypes
     content: Any
     mime_type: str | None
--- llmbrix/msg/model_msg_segment_types.py
+++ llmbrix/msg/model_msg_segment_types.py
@@ -11,4 +11,8 @@ class ModelMsgSegmentTypes(Enum):
     IMAGE = "image"
     THOUGHT = "thought"
     AUDIO = "audio"
+    FILE_URI = "file_uri"
+    CODE_EXECUTABLE = "code_executable"
+    CODE_RESULT = "code_result"
     UNSUPPORTED_FILE = "unsupported_file"
+    UNSUPPORTED_PART = "unsupported_part"
